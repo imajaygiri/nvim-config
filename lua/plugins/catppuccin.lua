9b6e233884c7ee3c90ae785f3e0fa61ad4559970
Ajay_Giri <imlearningcoding2024@gmail.com>
2025-09-12 19:33:26 +0530

    update config log: 0732

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,25 +9,25 @@ return {
                  light = "latte",
                  dark = "macchiato",
              },
-             -- color_overrides = {
-             --     mocha = {
-             --         base = "#000000",
-             --         mantle = "#000000",
-             --         crust = "#000000",
-             --     },
-             -- },
-             -- custom_highlights = function(colors)
-             --     return {
-             --         Normal = { bg = "#000000" },
-             --         NormalNC = { bg = "#000000" },
-             --         SignColumn = { bg = "#000000" },
-             --         VertSplit = { bg = "#000000", fg = "#000000" },
-             --         StatusLine = { bg = "#000000", fg = colors.text },
-             --         StatusLineNC = { bg = "#000000", fg = colors.overlay0 },
-             --         WinSeparator = { fg = "#000000" },
-             --         CursorLine = { bg = "#0a0a0a" },
-             --     }
-             -- end,
+             color_overrides = {
+                 mocha = {
+                     base = "#000000",
+                     mantle = "#000000",
+                     crust = "#000000",
+                 },
+             },
+             custom_highlights = function(colors)
+                 return {
+                     Normal = { bg = "#000000" },
+                     NormalNC = { bg = "#000000" },
+                     SignColumn = { bg = "#000000" },
+                     VertSplit = { bg = "#000000", fg = "#000000" },
+                     StatusLine = { bg = "#000000", fg = colors.text },
+                     StatusLineNC = { bg = "#000000", fg = colors.overlay0 },
+                     WinSeparator = { fg = "#000000" },
+                     CursorLine = { bg = "#0a0a0a" },
+                 }
+             end,
          })
 
          vim.cmd.colorscheme("catppuccin")
