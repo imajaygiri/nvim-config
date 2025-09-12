4e5514fe50e48447a2c801aae508bf07a7df6708
Ajay_Giri <imlearningcoding2024@gmail.com>
2025-09-12 19:35:12 +0530

    update config log: 0732

--- lua/plugins/color-square-box.lua
+++ lua/plugins/color-square-box.lua
@@ -1,17 +1,16 @@
 -- Plugin (Lazy.nvim)
-return{
-  "NvChad/nvim-colorizer.lua",
-  opts = {
-    filetypes = { "*" },
-    user_default_options = {
-      tailwind = true,
-      css = true,
-      names = true,
-      mode = "background", -- or 'foreground'
-    },
-  },
-  config = function(_, opts)
-    require("colorizer").setup(opts)
-  end,
+return {
+	"NvChad/nvim-colorizer.lua",
+	opts = {
+		filetypes = { "*" },
+		user_default_options = {
+			tailwind = true,
+			css = true,
+			names = true,
+			mode = "background", -- or 'foreground'
+		},
+	},
+	config = function(_, opts)
+		require("colorizer").setup(opts)
+	end,
 }
-
